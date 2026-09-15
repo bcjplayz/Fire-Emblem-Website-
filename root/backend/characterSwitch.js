a29f7a1e5a4ad4226a06f60992136c042a016acf
bcjplayz <bcjplayz@gmail.com>
2026-09-15 20:32:52 +0000

    IAMLOSINGIT

--- root/backend/characterSwitch.js
+++ root/backend/characterSwitch.js
@@ -1,6 +1,6 @@
 const image_data = [
-  { alt: ["Marth", "Lucina"], options: ["/root/img/Marth.png", "/root/img/lucina.png"], name: ["Marth", "Lucina"] },
-  { alt: ["Robin", "RobinF"], options: ["/root/img/Robin.png", "/root/img/RobinF.png"], name: ["Robin", "Robin"] },
+  { alt: ["Marth", "Lucina"], options: ["../img/Marth.png", "../img/lucina.png"], name: ["Marth", "Lucina"] },
+  { alt: ["Robin", "RobinF"], options: ["../img/Robin.png", "../img/RobinF.png"], name: ["Robin", "Robin"] },
 ];
 
 function getFileName(src) {
