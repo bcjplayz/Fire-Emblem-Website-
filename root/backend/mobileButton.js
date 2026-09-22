5ddc0b36b656a5a5c48dcf01e685da0c4b964e23
bcjplayz <bcjplayz@gmail.com>
2026-09-22 11:53:11 +0000

    degub config

--- root/backend/mobileButton.js
+++ root/backend/mobileButton.js
@@ -5,6 +5,7 @@ if (navToggle && siteNav) {
     const toggleMenu = () => {
         const isOpen = siteNav.classList.toggle('is-open');
         navToggle.setAttribute('aria-expanded', String(isOpen));
+        document.getElementById("console").textContent += "worked?S"
     };
 
     navToggle.addEventListener('click', toggleMenu);
